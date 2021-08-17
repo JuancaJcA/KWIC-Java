0410ad962c3977e450961ade00b9ea6303101b17
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-08-17 11:04:09 -0400

    Started the shifter

--- src/main/java/com/huachimingos/kwic/main.java
+++ src/main/java/com/huachimingos/kwic/main.java
@@ -1,8 +1,12 @@
 
 package com.huachimingos.kwic;
 
+import com.huachimingos.kwic.kwic.CircularShifter;
+
 public class main {
     public static void main(String[] args){
-        System.out.println("Jojos");
+        String[] data = {"Cien años de soledad", "El señor de los anillos", "Don Quijote de la Mancha"};
+        CircularShifter shifter = new CircularShifter(data);
+        //System.out.println("Jojos");
     }
 }
